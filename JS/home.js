35f415dc885a5fe6cf1ef261383d64e919512ef0
hibamajeed2007-lgtm <hiba.majeed2007@gmail.com>
2026-04-17 19:05:49 +0500

    commit

--- JS/home.js
+++ JS/home.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-    // Show popup after 2 seconds
+
     setTimeout(function () {
         $("#popup-overlay").fadeIn();
         $("#popup-modal").fadeIn();
     }, 2000);
 
-    // Close popup
+
     $("#close-btn, #popup-overlay").click(function () {
         $("#popup-overlay").fadeOut();
         $("#popup-modal").fadeOut();
@@ -19,26 +19,11 @@ $(document).ready(function () {
         }, 800);
     });
 
-    // Hover effect on destinations
+
     $(".destinations li").hover(function () {
         $(this).css("color", "#327D81");
     }, function () {
         $(this).css("color", "black");
     });
 
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-    new Splide(".splide", {
-        type: "loop",
-        perPage: 3,
-        gap: "20px",
-        autoplay: true,
-        pauseOnHover: true,
-
-        breakpoints: {
-            1024: { perPage: 2 },
-            768: { perPage: 1 },
-        },
-    }).mount();
 });
